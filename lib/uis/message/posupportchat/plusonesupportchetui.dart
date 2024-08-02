691aa974536476acc9095225cfff48158f128717
nbt <sainiprikshit18@gmail.com>
2024-08-02 09:50:16 +0530

    bugs fix

--- lib/uis/message/posupportchat/plusonesupportchetui.dart
+++ lib/uis/message/posupportchat/plusonesupportchetui.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:plusone/uis/components/custotextfield.dart';
+import 'package:plusone/utils/common.dart';
 import '../../../utils/colors.dart';
 
 class PlusOneSupportChetUi extends GetWidget{
@@ -23,7 +24,7 @@ class PlusOneSupportChetUi extends GetWidget{
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
                SizedBox(
-                height: h*.005,
+                height: 15,
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 5),
@@ -31,21 +32,7 @@ class PlusOneSupportChetUi extends GetWidget{
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    InkWell(
-                      onTap: () {
-                        Get.back();
-                      },
-                      child: Container(
-                        width: h*0.05,
-                        height:h*0.05,
-                        padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                        decoration: BoxDecoration(
-                            color: clrGreyLight,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: const Center(child: Icon(Icons.arrow_back_ios)),
-                      ),
-                    ),
+                    CommonUi.appBar(),
                     Column(
                       children: [
                         SizedBox(
