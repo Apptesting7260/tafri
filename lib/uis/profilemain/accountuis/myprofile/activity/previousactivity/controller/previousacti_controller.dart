f2d618ea58d545fb22f743754309dc6602555457
YCS <testdemo7260@gmail.com>
2024-09-04 18:08:49 +0530

    edit

--- lib/uis/profilemain/accountuis/myprofile/activity/previousactivity/controller/previousacti_controller.dart
+++ lib/uis/profilemain/accountuis/myprofile/activity/previousactivity/controller/previousacti_controller.dart
@@ -313,7 +313,7 @@ class PreviousActiController extends GetxController{
   Future<void> attlistapi(String? actid) async{
 
     Map body = {
-      'activity_id': 5,
+      'activity_id': actid,
       // 'user_id': LocalStorage.getUid()
     };
 
